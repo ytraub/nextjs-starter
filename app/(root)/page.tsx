6e0321d0be8c92b9d70f03529d72af13ecedc2e9
ytraub <y.traub@outlook.com>
2024-07-07 18:50:12 +0200

    Hono api tests

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import { useState } from "react";
 
 export default function Home() {
-  return <UserButton afterSwitchSessionUrl="/" />;
+  const [message, setMessage] = useState("");
+
+  const fetchFromServer = async () => {
+    const req = await fetch("/api/hello/BBBB");
+    const res = await req.json();
+
+    setMessage(res.test);
+  };
+
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center flex-col gap-4">
+      <UserButton />
+      <p className="text-sm text-muted-foreground">{message}</p>
+      <Button onClick={fetchFromServer}>Fetch from server</Button>
+    </div>
+  );
 }
